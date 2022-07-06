0dfc00a9efa37021ae13bcade32b3c9155f0fa0e
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-07-06 12:42:10 +0300

    SQL-I 3rd day reformatted code style

--- SQL I/Day 3 String Processing Functions/Fix Names in a Table.sql
+++ SQL I/Day 3 String Processing Functions/Fix Names in a Table.sql
@@ -1,8 +1,5 @@
-SELECT
-    user_id,
-    CONCAT(UPPER(SUBSTRING(name, 1, 1)),
-    LOWER(SUBSTRING(name, 2))) AS name
-FROM
-    Users
-ORDER BY
-    user_id;
+SELECT user_id,
+       CONCAT(UPPER(SUBSTRING(name, 1, 1)),
+       LOWER(SUBSTRING(name, 2))) AS name
+  FROM Users
+ ORDER BY user_id;
--- SQL I/Day 3 String Processing Functions/Patients With a Condition.sql
+++ SQL I/Day 3 String Processing Functions/Patients With a Condition.sql
@@ -1,6 +1,3 @@
-SELECT
-    *
-FROM
-    Patients
-WHERE
-    conditions LIKE '% DIAB1%' OR conditions LIKE 'DIAB1%';
+SELECT *
+  FROM Patients
+ WHERE conditions REGEXP '\\bDIAB1';
