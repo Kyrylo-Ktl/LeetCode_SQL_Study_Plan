620ffb503972bb959988062f74efe5291ae0327b
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-07-06 12:50:58 +0300

    SQL-I 1st day reformatted code style

--- SQL I/Day 1 Select/Customers Who Never Order.sql
+++ SQL I/Day 1 Select/Customers Who Never Order.sql
@@ -1,12 +1,6 @@
-SELECT
-    name AS 'Customers'
-FROM
-    Customers
-WHERE
-    id not in
-    (
-        SELECT
-            customerId
-        FROM
-            Orders
-    );
+SELECT name AS 'Customers'
+  FROM Customers
+ WHERE id NOT IN (
+    SELECT customerId
+      FROM Orders
+);
