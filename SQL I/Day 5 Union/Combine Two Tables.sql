d73daccd0d3b2494da53a0608506d273cfd2d38a
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-07-06 12:25:48 +0300

    SQL-I 5th day reformatted code style

--- SQL I/Day 5 Union/Combine Two Tables.sql
+++ SQL I/Day 5 Union/Combine Two Tables.sql
@@ -1,4 +1,6 @@
-SELECT
-    FirstName, LastName, City, State
-FROM
-    Person LEFT JOIN Address USING (PersonId);
+SELECT FirstName,
+       LastName,
+       City,
+       State
+  FROM Person AS p LEFT JOIN Address AS a
+    ON p.PersonId = a.PersonId;
--- SQL I/Day 5 Union/Customer Who Visited but Did Not Make Any Transactions.sql
+++ SQL I/Day 5 Union/Customer Who Visited but Did Not Make Any Transactions.sql
@@ -1,9 +1,6 @@
-SELECT
-	customer_id,
-	COUNT(*) AS count_no_trans
-FROM
-	Visits LEFT JOIN Transactions USING (visit_id)
-WHERE
-	Transactions.visit_id IS NULL
-GROUP BY
-    customer_id;
+SELECT v.customer_id,
+       COUNT(*) AS count_no_trans
+  FROM Visits AS v LEFT JOIN Transactions AS t
+    ON v.visit_id = t.visit_id
+ WHERE t.visit_id IS NULL
+ GROUP BY customer_id;
