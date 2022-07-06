39fd31524cd034fc58e0935cda61e01ea043908c
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-07-06 12:07:01 +0300

    SQL-I 9th day reformatted code style

--- SQL I/Day 9 Control of Flow/Capital Gain Loss.sql
+++ SQL I/Day 9 Control of Flow/Capital Gain Loss.sql
@@ -1,7 +1,4 @@
-SELECT
-    stock_name,
-    SUM(IF(operation='Buy', -price, price)) AS capital_gain_loss
-FROM
-    Stocks
-GROUP BY
-    stock_name;
+SELECT stock_name,
+       SUM(IF(operation='Buy', -price, price)) AS capital_gain_loss
+  FROM Stocks
+ GROUP BY stock_name;
