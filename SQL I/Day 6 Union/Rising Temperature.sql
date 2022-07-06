dfabd48f0f08ab289852b9274db3bd4f40b4e321
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-07-06 12:20:32 +0300

    SQL-I 6th day reformatted code style

--- SQL I/Day 6 Union/Rising Temperature.sql
+++ SQL I/Day 6 Union/Rising Temperature.sql
@@ -1,9 +1,5 @@
-SELECT
-    DISTINCT a.Id
-FROM
-    Weather a,
-    Weather b
-WHERE
-    a.temperature > b.temperature
-    AND
-    DATEDIFF(a.recordDate, b.recordDate) = 1;
+SELECT DISTINCT a.Id
+  FROM Weather AS a,
+       Weather AS b
+ WHERE a.temperature > b.temperature
+   AND DATEDIFF(a.recordDate, b.recordDate) = 1;
